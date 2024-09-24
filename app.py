5e48f2db6509a96a3ced88dc533a96ecec23efd3
Colin101-design <colin@ukled-ltd.co.uk>
2024-09-24 23:15:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,24 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    """Render the home page."""
+def homepage():
     return render_template('index.html')
 
 @app.route('/about')
 def about():
-    """Render the about us page."""
     return render_template('about.html')
-
-@app.route('/products')
-def products():
-    """Render the products page."""
-    return render_template('products.html')
-
-@app.route('/contact')
-def contact():
-    """Render the contact page."""
-    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
